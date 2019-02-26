b3510961b8505399d333e16acb54fbb21a00e53a
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2019-02-26 11:07:03 +0100

    renew copyright hints

--- Classes/Helper/GetEventHelper.php
+++ Classes/Helper/GetEventHelper.php
@@ -1,13 +1,14 @@
 <?php
-/**
- * @package     con4gis
- * @filesource  GetEventHelper.php
- * @version     1.0.0
- * @since       29.09.17 - 12:20
- * @author      Patrick Froch <info@easySolutionsIT.de>
- * @link        http://easySolutionsIT.de
- * @copyright   e@sy Solutions IT 2017
- * @license     EULA
+/*
+ * This file is part of con4gis,
+ * the gis-kit for Contao CMS.
+ *
+ * @package    con4gis
+ * @version    6
+ * @author     con4gis contributors (see "authors.txt")
+ * @license    LGPL-3.0-or-later
+ * @copyright  Küstenschmiede GmbH Software & Design
+ * @link       https://www.con4gis.org
  */
 namespace con4gis\ExportBundle\Classes\Helper;
 

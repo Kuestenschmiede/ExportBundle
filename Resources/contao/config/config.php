45ddbad3778226c83edc5ba1bae32167ad8a04be
coastforge-cro <claudio.ross@kuestenschmiede.de>
2019-03-07 10:37:10 +0100

    Change backend structure

--- Resources/contao/config/config.php
+++ Resources/contao/config/config.php
@@ -13,15 +13,15 @@
 
 $GLOBALS['con4gis']['export']['installed'] = true;
 
-$GLOBALS['BE_MOD']['con4gis'] = array_merge($GLOBALS['BE_MOD']['con4gis'], array(
+$GLOBALS['BE_MOD']['con4gis_core'] = array_merge($GLOBALS['BE_MOD']['con4gis_core'], array(
     'export' => array(
         'tables'        => array('tl_c4g_export'),
         'runexport'     => array('\con4gis\ExportBundle\Classes\Contao\Modules\ModulExport', 'runExport')
     )
 ));
 
-$GLOBALS['BE_MOD']['con4gis'] =
-    \con4gis\CoreBundle\Resources\contao\classes\C4GUtils::sortBackendModules($GLOBALS['BE_MOD']['con4gis']);
+//$GLOBALS['BE_MOD']['con4gis'] =
+//    \con4gis\CoreBundle\Resources\contao\classes\C4GUtils::sortBackendModules($GLOBALS['BE_MOD']['con4gis']);
 /**
  * EXPORT-SETTINGS
  */
